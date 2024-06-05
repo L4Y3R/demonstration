ceb8b3f829f26ba34eb8b4fecc5c58ea17b7fd97
Nipun Balage <105732109+L4Y3R@users.noreply.github.com>
2024-06-05 08:30:53 +0530

    remove commented segement

--- src/main/java/com/example/demonstration/service/DeviceService.java
+++ src/main/java/com/example/demonstration/service/DeviceService.java
@@ -6,8 +6,6 @@ import com.example.demonstration.repository.DeviceRepo;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
