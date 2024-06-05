a03a69c8e409e956a877e9f6ebf8f728299ffc49
Shelomi201031 <shelomi20nipunika@gmail.com>
2024-06-05 10:04:38 +0530

    DeviceGroupDTO and UserDTO Implemented

---
+++ src/main/java/com/example/demonstration/dto/DeviceGroupDTO.java
@@ -0,0 +1,16 @@
+package com.example.demonstration.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class DeviceGroupDTO {
+    private String groupName;
+    private String ownerName;
+}
