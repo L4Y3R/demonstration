dc8d0a41133f7dfaacacec5ac7910bd442dfae8c
Shanoli Silva <shanoli.silva@zorrosign.com>
2024-06-05 09:28:49 +0530

    User Entity Added

---
+++ src/main/java/com/example/demonstration/entity/User.java
@@ -0,0 +1,25 @@
+package com.example.demonstration.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection="user")
+
+public class User {
+    @Id
+    private String userId;
+    private String username;
+    @javax.persistence.Id
+    private Long Id;
+}
