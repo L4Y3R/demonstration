c9888f4c22a3754f99f1fd7fa0805e0d67c6f3e9
Nipuni-De-Silva <nipunisha274@gmail.com>
2024-06-05 10:44:14 +0530

    Updated UserService

--- src/main/java/com/example/demonstration/service/UserService.java
+++ src/main/java/com/example/demonstration/service/UserService.java
@@ -1,4 +1,64 @@
 package com.example.demonstration.service;
 
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public class UserService {
+    @Autowired
+    private UserRepo userRepo;
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public List<UserDTO> getAllUsers(){
+        List<User> users = userRepo.findAll();
+        return modelMapper.map(users, new TypeToken<List<UserDTO>>(){}.getType());
+    }
+
+    public UserDTO getUser(String id){
+        optional<User> userOptional = userRepo.findById(id);
+        if(userOptional.isPresent()){
+            return modelMapper.map(userOptional.get(), UserDTO.class);
+        }else {
+            return null;
+        }
+    }
+
+    public UserDTO createUser(UserDTO userDTO){
+        userRepo.save(modelMapper.map(userDTO, User.class));
+        return userDTO;
+    }
+
+    public UserDTO updateUser(String id, UserDTO userDTO){
+        optional<User> existingUserOptional = userRepo.findById(id);
+        if(existingUserOptional.isPresent()){
+            User user = existingUserOptional.get();
+            modelMapper.map(userDTO, user);
+            user.setUserId(id);
+            UserRepo.save(user);
+            return userDTO;
+        }else {
+            return null;
+        }
+    }
+
+    public Optional<UserDTO> deleteUser(String id) {
+        Optional<User> existingUserOptional = userRepo.findById(id);
+        if (existingUserOptional.isPresent()) {
+            userRepo.deleteById(id);
+            UserDTO deletedDeviceDTO = new UserDTO();
+            deletedUserDTO.setUserName(existingUserOptional.get().getUserName());
+            deletedUserDTO.setUserType(existingUserOptional.get().getUserType());
+            deletedUserDTO.setStatus(existingUserOptional.get().getStatus());
+            return Optional.of(deletedUserDTO);
+        } else {
+            return Optional.empty();
+        }
+    }
+
+
 }
