15c0e736a22aa96969f327e5ac39e31584252d4e
Nipun Balage <105732109+L4Y3R@users.noreply.github.com>
2024-06-07 08:23:11 +0530

    remove unused imports

--- src/main/java/com/example/demonstration/dto/DeviceGroupDTO.java
+++ src/main/java/com/example/demonstration/dto/DeviceGroupDTO.java
@@ -6,9 +6,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.CascadeType;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
 import java.util.List;
 
 @AllArgsConstructor
